ede94ccfb06259dfa010e30199aece51868ec373
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-08-12 21:49:52 +0700

    Remove unused templates and views; update base styles and layout for improved UI/UX

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact'),   
     path('for/', views.for_view, name='for'),
     path('multipli/', views.multipli, name='multipli'),
 ]

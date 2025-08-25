c70d38b5c55304ff69e5e847695f2b9a1813dff2
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-08-25 10:44:04 +0700

    Add Student model and admin registration for improved data management

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Student)
+class StudentsAdmin(admin.ModelAdmin):
+    list_display = ['stid', 'neme_prefix', 'first_name', 'last_neme']
+    search_fields = ['first_name', 'last_neme']    
+    
--- webpage/models.py
+++ webpage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+PREFIX_CHOICES =[
+    ('นาย', 'นาย'),
+    ('นาง', 'นาง'),
+    ('นางสาว', 'นางสาว'),
+    
+]
+class Student(models.Model):
+    stid = models.IntegerField(unique=True)
+    neme_prefix = models.CharField(choices=PREFIX_CHOICES, max_length=10)
+    first_name = models.CharField(max_length=100)
+    last_neme = models.CharField(max_length=100)
+    age = models.IntegerField(default=18)
+    
+def __str__(self):
+        return str(self.stid)
+    
